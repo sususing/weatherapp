f1dfe5cc08652bce7ca4e7fa4943aa0276d15185
Sudhanshu kumar singh <127447904+sususing@users.noreply.github.com>
2023-08-08 12:53:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const weather_body = document.querySelector('.weather-body');
 
 
 async function checkWeather(city){
-    const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
+    const api_key = "d9dc3319dd8db285035f3b9f4b4f2432";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
